d32f5109b4ade04cbe6f797931c3f10a81e7bd61
PhilipLyonsGH <philiplyons@gmail.com>
2026-07-07 11:22:01 +0100

    Add portfolio image data

---
+++ src/data/portfolio.js
@@ -0,0 +1,12 @@
+export const portfolio = [
+  { image: 'Amy.jpg', title: 'Corporate Portrait', category: 'Photography' },
+  { image: 'AnvilFashion.jpg', title: 'Fashion Photography', category: 'Photography' },
+  { image: 'DewaltSaw.jpg', title: 'Product & Brand Photography', category: 'Photography' },
+  { image: 'Eurofighters.jpg', title: 'Aerospace', category: 'Photography' },
+  { image: 'F15.jpg', title: 'Aviation', category: 'Photography' },
+  { image: 'GirlPortrait.jpg', title: 'Portrait', category: 'Photography' },
+  { image: 'Miami.jpg', title: 'Location Photography', category: 'Photography' },
+  { image: 'PulsarFashion.jpg', title: 'Fashion & Lifestyle', category: 'Photography' },
+  { image: 'RallyCar.jpg', title: 'Automotive', category: 'Photography' },
+  { image: 'Schipol.jpg', title: 'Industrial & Infrastructure', category: 'Photography' }
+];
